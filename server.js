d10aa839ceb2ec6c8e4c1e737a5698e61768ae8b
anaswail <anaswail246@gmail.com>
2024-12-02 20:41:17 +0200

    creative AMO

--- server.js
+++ server.js
@@ -10,41 +10,26 @@ import { connectDb } from "./config/db.js";
 import users from "./routes/users.js";
 import courses from "./routes/courses.js";
 import admins from "./routes/admins.js";
-import { verifyAdminToken, addAdmin } from "./controllers/admins.js";
+import { verifyAdminToken, addAdmin } from "./controlers/admins.js";
+import User from "./models/User.js";
+import Admin from "./models/Admin.js";
 
 // Initialize app and environment variables
 dotenv.config({ path: "./config/config.env" });
 const app = express();
-const PORT = process.env.PORT || 22756;
+const PORT = process.env.PORT || 3000 || 3001 || 9000;
 
 // Middleware setup
 app.use(express.json());
 app.use(morgan("dev"));
-
-// Configure CORS
 app.use(
   cors({
-    origin: [
-      "https://thoughtless-effectst.surge.sh", // Frontend URL
-      "http://localhost:3000", // Local development URL
-    ],
+    origin: "https://creative-amo.vercel.app/", // Allow the frontend origin
     credentials: true, // Allow cookies and credentials
     allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Specify allowed methods
-    optionsSuccessStatus: 204, // Prevent OPTIONS response redirects
+    methods: "GET, POST, PUT, DELETE,OPTIONS", // Specify allowed methods
   })
 );
-
-// Avoid redirects for HTTPS requests
-app.enable("trust proxy"); // For proxies (e.g., Vercel)
-// Force HTTPS if required
-app.use((req, res, next) => {
-  if (req.secure || req.headers["x-forwarded-proto"] === "https") {
-    next();
-  } else {
-    res.status(403).send("HTTPS Required");
-  }
-});
 
 // Routes
 app.get("/api/v1/admin/check", verifyAdminToken, (req, res) => {
@@ -56,7 +41,7 @@ app.get("/api/v1/admin/check", verifyAdminToken, (req, res) => {
 
 app.post("/api/v1/admin/add", async (req, res) => {
   const admin = await addAdmin(req, res);
-  res.json(admin);
+  res = admin;
 });
 
 app.use("/api/v1/users", users);
