274cdcc56d49c08dcdc1d0ba5a46296849e1f77c
moh-sa <tno.rappelz@gmail.com>
2024-12-03 16:52:54 +0300

    test2

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 dotenv.config();
 
-const dbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cf6q7.mongodb.net`;
+const dbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cf6q7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 console.log(process.env.DB_USERNAME);
 console.log(process.env.DB_PASSWORD);
