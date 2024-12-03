7925c0dc1991847c84bda659fe3d432b89725a15
moh-sa <tno.rappelz@gmail.com>
2024-12-03 16:51:15 +0300

    test

--- config/db.js
+++ config/db.js
@@ -2,7 +2,11 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 dotenv.config();
 
-const dbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3ntop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const dbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cf6q7.mongodb.net`;
+
+console.log(process.env.DB_USERNAME);
+console.log(process.env.DB_PASSWORD);
+console.log(dbUrl);
 
 export const connectDb = async () => {
   try {
