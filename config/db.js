2b742ea5e8f8fc3bafa58336373c73f7a3c100b1
anaswail <anaswail246@gmail.com>
2024-12-03 20:52:22 +0200

    fix the host error in the backend

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 dotenv.config();
 
-const dbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3ntop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const dbUrl = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3ntop.mongodb.net`;
 
 export const connectDb = async () => {
   try {
